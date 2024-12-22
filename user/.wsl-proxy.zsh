9c137b2fbed1277b408dcfe9ca07cd859b571a6c
Euoao <euoao-ye@qq.com>
2024-12-22 20:30:15 +0800

    update proxy

--- user/.wsl-proxy.zsh
+++ user/.wsl-proxy.zsh
@@ -6,9 +6,9 @@ proxy() {
   export proxyPort=7897 #端口
   export https_proxy="http://${hostip}:${proxyPort}";
   export http_proxy="http://${hostip}:${proxyPort}";
-  export all_proxy="http://${hostip}:${proxyPort}";
-  git config --global http.https://github.com.proxy ${PROXY_HTTP}
-  git config --global https.https://github.com.proxy ${PROXY_HTTP}
+  export all_proxy="socks5://${hostip}:${proxyPort}";
+  # git config --global http.https://github.com.proxy ${PROXY_HTTP}
+  # git config --global https.https://github.com.proxy ${PROXY_HTTP}
   # echo -e "Acquire::http::Proxy \"http://${hostip}:${proxyPort}\";" | sudo tee -a /etc/apt/apt.conf.d/proxy.conf > /dev/null;
   # echo -e "Acquire::https::Proxy \"http://${hostip}:${proxyPort}\";" | sudo tee -a /etc/apt/apt.conf.d/proxy.conf > /dev/null;
   echo "Proxy has been opened."
@@ -18,10 +18,9 @@ unproxy() {
   unset https_proxy;
   unset http_proxy;
   unset all_proxy;
-  git config --global --unset http.https://github.com.proxy
-  git config --global --unset https.https://github.com.proxy
+  # git config --global --unset http.https://github.com.proxy
+  # git config --global --unset https.https://github.com.proxy
   # sudo sed -i -e "/Acquire::http::Proxy/d" /etc/apt/apt.conf.d/proxy.conf;
   # sudo sed -i -e "/Acquire::https::Proxy/d" /etc/apt/apt.conf.d/proxy.conf;
   echo "Proxy has been closed."
 }
-
